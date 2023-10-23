dadfccff22be931433aa1d9349fd9b81da34d82b
Yewen Zhou <yewen.zhou@scryanalytics.com>
2023-10-23 14:02:02 -0700

    Fix issue for not able to evaluate zip

--- dataset/TD500/eval.sh
+++ dataset/TD500/eval.sh
@@ -1,6 +1,5 @@
 cd dataset/TD500
 rm submit.zip
 cp $1/*.txt submit
-cd submit/;zip -r  submit.zip * &> ../log.txt ; mv ./submit.zip ../; cd ../
-rm log.txt
+cd submit/;zip -r  submit.zip *; mv ./submit.zip ../; cd ../
 python Evaluation_Protocol/script.py -g=gt.zip -s=submit.zip
